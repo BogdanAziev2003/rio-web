a03318d4f3a2604dbf95d014095e54abb3d38e62
Zaur Taysaev <z.taysaev2003@mail.ru>
2024-01-31 02:42:49 +0300

    border none on telegram web

--- src/components/ModalWindow/index.jsx
+++ src/components/ModalWindow/index.jsx
@@ -30,7 +30,7 @@ const ModalWindow = ({
 
   return (
     <Modal open={open} onClose={handleClose}>
-      <Box sx={{ borderRadius: '15px' }}>
+      <Box sx={{ borderColor: '#f00' }}>
         <div className={styles.modal}>
           <IoMdCloseCircle
             className={styles.modal__close__btn}
