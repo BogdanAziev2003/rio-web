f11f0fc79894db95346cd7e151767c4f5e2b3b0a
Zaur Taysaev <z.taysaev2003@mail.ru>
2024-01-30 16:18:35 +0300

    add autocomplited address logic

--- src/pages/Cart/Delivery/Address.jsx
+++ src/pages/Cart/Delivery/Address.jsx
@@ -1,20 +1,72 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from '../Cart.module.scss';
 
 const Addres = () => {
-  return (
-    <div className={styles.adress}>
-      <div className={styles.adress__text}>
-        <p>Выберите город и улицу</p>
-      </div>
+  var options = {
+    fields: [
+      { id: 'js-Field1', levels: ['Region', 'District'] },
+      { id: 'js-Field2', levels: ['City', 'Place'] },
+      { id: 'js-Field3', levels: ['Site', 'Street', 'House'] },
+    ],
+  };
 
-      <div className={styles.adress__list}>
-        <div className={styles.adress__input}>
-          <input type="text" className={styles.input} placeholder="Город" />
+  let url =
+    'https://ahunter.ru/site/cleanse/address?user=BogdanAzievnPfqPINLnk9jt6yTgS1ls4&output=json&query=';
+
+  window.AhunterSuggest.Address.Discrete(options);
+
+  const getCoordinats = async () => {
+    let inputs = document.querySelectorAll('.input-wrapper input');
+    let address = '';
+    inputs.forEach((input) => {
+      address += input.value + ' ';
+    });
+    address = encodeURI(address.trim());
+
+    await fetch(url + address)
+      .then((res) => {
+        let json = res.json();
+        return json;
+      })
+      .then((data) => {
+        console.log(data);
+        console.log(data.addresses[0].geo_data.mid.lat);
+        console.log(data.addresses[0].geo_data.mid.lon);
+      });
+  };
+
+  return (
+    <div className="input-wrapper">
+      <div className={styles.adress}>
+        <div className={styles.adress__text}>
+          <p>Выберите город и улицу</p>
         </div>
-        <div className={styles.adress__input}>
-          <input type="text" className={styles.input} placeholder="Улица" />
+        <div className={styles.adress__list}>
+          <div className={styles.adress__input}>
+            <input
+              className={styles.input}
+              id="js-Field2"
+              placeholder="Город    "
+            />
+          </div>
+          <div className={styles.adress__input}>
+            <input
+              className={styles.input}
+              id="js-Field3"
+              placeholder="Улица, дом"
+            />
+          </div>
+          <input
+            className={styles.visibleFalse}
+            id="js-Field1"
+            placeholder="Регион, райнон"
+            value="Респ Северная Осетия - Алания, р-н Правобережный"
+            onChange={() => {}}
+          />
         </div>
+        <button onClick={getCoordinats} className="get-coordinates">
+          Получить координаты
+        </button>
       </div>
     </div>
   );
