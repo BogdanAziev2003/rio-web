32fb2e70c243e40aa7d0b326d641e4fbdd6ad624
Zaur Taysaev <z.taysaev2003@mail.ru>
2024-01-31 18:13:13 +0300

    optimization navbar and main page

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -1,162 +1,38 @@
-import React, { useEffect, useState } from 'react';
-
+import React from 'react';
 import styles from './Navbar.module.scss';
 import { Link } from 'react-router-dom';
 
 const Navbar = ({ pathname }) => {
-  const [active, setActive] = useState(`${pathname}`);
-
-  useEffect(() => {
-    pathname === '/' && setActive('Все');
-  }, [pathname]);
+  const navItems = [
+    { path: '/', title: 'Все' },
+    { path: '/burgers', title: 'Бургеры' },
+    { path: '/shaurma', title: 'Шаурма' },
+    { path: '/tauk', title: 'Тауки' },
+    { path: '/hot-dogs', title: 'Хот-Доги' },
+    { path: '/sandwich', title: 'Сэндвичи' },
+    { path: '/firmen', title: 'Фирменые' },
+    { path: '/snack', title: 'Снэки' },
+    { path: '/sauce', title: 'Соусы' },
+    { path: '/coffee', title: 'Кофе' },
+    { path: '/tea', title: 'Чаи' },
+  ];
 
   return (
     <div className={styles.nav}>
       <div className={styles.nav__inner}>
         <div className={styles.nav__list}>
-          <div
-            className={
-              active === 'Все'
-                ? `${styles.nav__el} ${styles.nav__el_active}`
-                : styles.nav__el
-            }
-          >
-            <p>
-              <Link to="/" onClick={() => setActive('Все')}>
-                Все
-              </Link>
-            </p>
-          </div>
-          <div
-            className={
-              active === '/burgers'
-                ? `${styles.nav__el} ${styles.nav__el_active}`
-                : styles.nav__el
-            }
-          >
-            <p>
-              <Link to="/burgers" onClick={() => setActive('/burgers')}>
-                Бургеры
-              </Link>
-            </p>
-          </div>
-          <div
-            className={
-              active === '/shaurma'
-                ? `${styles.nav__el} ${styles.nav__el_active}`
-                : styles.nav__el
-            }
-          >
-            <p>
-              <Link to="/shaurma" onClick={() => setActive('/shaurma')}>
-                Шаурма
-              </Link>
-            </p>
-          </div>
-          <div
-            className={
-              active === '/tauk'
-                ? `${styles.nav__el} ${styles.nav__el_active}`
-                : styles.nav__el
-            }
-          >
-            <p>
-              <Link to="/tauk" onClick={() => setActive('/tauk')}>
-                Тауки
-              </Link>
-            </p>
-          </div>
-          <div
-            className={
-              active === '/hot-dogs'
-                ? `${styles.nav__el} ${styles.nav__el_active}`
-                : styles.nav__el
-            }
-          >
-            <p>
-              <Link to="/hot-dogs" onClick={() => setActive('/hot-dogs')}>
-                Хот-Доги
-              </Link>
-            </p>
-          </div>
-          <div
-            className={
-              active === '/sandwich'
-                ? `${styles.nav__el} ${styles.nav__el_active}`
-                : styles.nav__el
-            }
-          >
-            <p>
-              <Link to="/sandwich" onClick={() => setActive('/sandwich')}>
-                Сэндвичи
-              </Link>
-            </p>
-          </div>
-          <div
-            className={
-              active === '/firmen'
-                ? `${styles.nav__el} ${styles.nav__el_active}`
-                : styles.nav__el
-            }
-          >
-            <p>
-              <Link to="/firmen" onClick={() => setActive('/firmen')}>
-                Фирменые
-              </Link>
-            </p>
-          </div>
-          <div
-            className={
-              active === '/snack'
-                ? `${styles.nav__el} ${styles.nav__el_active}`
-                : styles.nav__el
-            }
-          >
-            <p>
-              <Link to="/snack" onClick={() => setActive('/snack')}>
-                Снэки
-              </Link>
-            </p>
-          </div>
-          <div
-            className={
-              active === '/sauce'
-                ? `${styles.nav__el} ${styles.nav__el_active}`
-                : styles.nav__el
-            }
-          >
-            <p>
-              <Link to="/sauce" onClick={() => setActive('/sauce')}>
-                Соусы
-              </Link>
-            </p>
-          </div>
-          <div
-            className={
-              active === '/coffee'
-                ? `${styles.nav__el} ${styles.nav__el_active}`
-                : styles.nav__el
-            }
-          >
-            <p>
-              <Link to="/coffee" onClick={() => setActive('/coffee')}>
-                Кофе
-              </Link>
-            </p>
-          </div>
-          <div
-            className={
-              active === 'tea'
-                ? `${styles.nav__el} ${styles.nav__el_active}`
-                : styles.nav__el
-            }
-          >
-            <p>
-              <Link to="/tea" onClick={() => setActive('tea')}>
-                Чаи
-              </Link>
-            </p>
-          </div>
+          {navItems.map((item) => (
+            <div
+              key={item.path}
+              className={`${styles.nav__el} ${
+                pathname === item.path ? styles.nav__el_active : ''
+              }`}
+            >
+              <p>
+                <Link to={item.path}>{item.title}</Link>
+              </p>
+            </div>
+          ))}
         </div>
       </div>
     </div>
--- src/pages/MainPage/index.jsx
+++ src/pages/MainPage/index.jsx
@@ -1,19 +1,37 @@
 import React from 'react';
-
 import styles from './MainPage.module.scss';
 import Item from 'components/Item';
 
 const MainPage = ({ items }) => {
+  const categories = [
+    { id: 0, title: 'Бургеры' },
+    { id: 1, title: 'Шаурма' },
+    { id: 2, title: 'Тауки' },
+    { id: 3, title: 'Хот-Доги' },
+    { id: 4, title: 'Сэндвичи' },
+    { id: 5, title: 'Фирменные' },
+    { id: 6, title: 'Снэки' },
+    { id: 7, title: 'Соусы' },
+    { id: 8, title: 'Кофе' },
+    { id: 9, title: 'Чаи' },
+  ];
+
   return (
     <div className={styles.main}>
-      <div className={styles.main__title}>
-        <p>Бургеры</p>
-      </div>
-      <div className={styles.main__list}>
-        {items.map((item, id) => (
-          <Item key={id} {...item} />
-        ))}
-      </div>
+      {categories.map((category) => (
+        <div key={category.id} className={styles.main__category}>
+          <div className={styles.main__title}>
+            <p>{category.title}</p>
+          </div>
+          <div className={styles.main__list}>
+            {items
+              .filter((item) => item.category === category.id)
+              .map((item, id) => (
+                <Item key={id} {...item} />
+              ))}
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
