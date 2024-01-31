693198269d5844a244d2b04b57d73c1f7c687762
Zaur Taysaev <z.taysaev2003@mail.ru>
2024-02-01 01:48:03 +0300

    try phone error logic

--- src/pages/Cart/index.jsx
+++ src/pages/Cart/index.jsx
@@ -48,7 +48,7 @@ const CartPage = () => {
   const { comment } = useSelector((state) => state.comment);
 
   const onSendData = useCallback(() => {
-    if (!phoneIsFalse) {
+    if (phoneIsFalse) {
       dispatch(setPhoneError(true));
       return;
     }
