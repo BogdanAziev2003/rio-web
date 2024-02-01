fb6acec4ea01aac5cbea43d54856dbed0f3e0c95
Zaur Taysaev <z.taysaev2003@mail.ru>
2024-02-01 16:33:13 +0300

    EmptyCart style add

--- src/pages/Cart/EmptyCart.jsx
+++ src/pages/Cart/EmptyCart.jsx
@@ -5,8 +5,8 @@ import styles from './Cart.module.scss';
 const EmptyCart = () => {
   return (
     <div className={styles.empty}>
-      <h3 className={styles.empty__title}>Корзина пуста</h3>
       <img src={cartImage} alt="empty-cart" className={styles.empty__image} />
+      <h3 className={styles.empty__title}>Корзина пуста </h3>
     </div>
   );
 };
