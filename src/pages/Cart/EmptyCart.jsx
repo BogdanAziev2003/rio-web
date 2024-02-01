afc1a50be03660bf229322fdcc03251f65cc3d58
Zaur Taysaev <z.taysaev2003@mail.ru>
2024-02-01 16:37:00 +0300

    EmptyCart text delete

--- src/pages/Cart/EmptyCart.jsx
+++ src/pages/Cart/EmptyCart.jsx
@@ -6,7 +6,6 @@ const EmptyCart = () => {
   return (
     <div className={styles.empty}>
       <img src={cartImage} alt="empty-cart" className={styles.empty__image} />
-      <h3 className={styles.empty__title}>Корзина пуста </h3>
     </div>
   );
 };
