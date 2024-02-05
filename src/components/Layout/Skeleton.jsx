96fe0c03cff34396ddcab4278f91cae9ef77b33d
Zaur Taysaev <z.taysaev2003@mail.ru>
2024-02-05 16:01:12 +0300

    skeleton style change

--- src/components/Layout/Skeleton.jsx
+++ src/components/Layout/Skeleton.jsx
@@ -4,15 +4,15 @@ import ContentLoader from 'react-content-loader';
 export const Skeleton = (props) => (
   <ContentLoader
     speed={2}
-    width={170}
-    height={300}
-    viewBox="0 0 180 300"
+    width={200}
+    height={220}
+    viewBox="0 0 200 300"
     backgroundColor="#f0f0f0"
-    foregroundColor="#e0e0e0" // Updated foregroundColor to a lighter color
+    foregroundColor="#e0e0e0"
     {...props}
   >
-    <rect x="0" y="0" rx="28" ry="28" width="170" height="164" />
-    <rect x="0" y="176" rx="11" ry="11" width="86" height="44" />
-    <rect x="0" y="237" rx="15" ry="15" width="170" height="47" />
+    <rect x="0" y="0" rx="28" ry="28" width="200" height="164" />
+    <rect x="0" y="176" rx="11" ry="11" width="86" height="45" />
+    <rect x="0" y="237" rx="15" ry="15" width="200" height="37" />
   </ContentLoader>
 );
